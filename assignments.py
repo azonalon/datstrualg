38e7a79ba6f4120971df5583d4972292a8a22090
azonalon <sauter.eduard@gmail.com>
2015-10-16 16:20:34 +0200

    Added pascal triangle

--- assignments.py
+++ assignments.py
@@ -1,11 +1,33 @@
-# Cell
 def Binomial(n,k):
     if k == 0 or n == k:
         return 1
     elif n<k:
-        raise Exception('Value Error')
+        raise Exception('First argument must be bigger than second')
     else:
         return Binomial(n-1, k-1) + Binomial(n-1, k)
 
-print(Binomial(5,3))
-# Cell
+def Fibonacci(n):
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    elif n <= 0:
+        raise Exception('Number must be positive')
+    else:
+        return Fibonacci(n-1)+Fibonacci(n-2)
+
+def Factorial(n):
+    if n == 0:
+        return 1
+    return n*Factorial(n-1)
+
+
+print('Pascal Triangle:')
+for n in range(0,10):
+    line=''
+    for j in range(0,n+1):
+        line += str(Binomial(n,j))+' '
+    print(line.center(80))
+
+print(Factorial(5))
+print([Fibonacci(n) for n in range(0,10)])
