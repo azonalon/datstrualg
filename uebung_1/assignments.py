644ec0e04e61bf3f41650fbbf19b56b0b45d223c
azonalon <sauter.eduard@gmail.com>
2015-10-23 15:37:55 +0200

    add 2

--- uebung_1/assignments.py
+++ uebung_1/assignments.py
@@ -1,10 +1,13 @@
-def Binomial(n,k):
+#!/bin/python
+
+def Binomial(n, k):
     if k == 0 or n == k:
         return 1
-    elif n<k:
+    elif n < k:
         raise Exception('First argument must be bigger than second')
     else:
         return Binomial(n-1, k-1) + Binomial(n-1, k)
+
 
 def Fibonacci(n):
     if n == 0:
@@ -14,15 +17,18 @@ def Fibonacci(n):
     elif n <= 0:
         raise Exception('Number must be positive')
     else:
-        return Fibonacci(n-1)+Fibonacci(n-2)
+        return Fibonacci(n-1) + Fibonacci(n-2)
+
 
 def Factorial(n):
     if n == 0:
         return 1
-    return n*Factorial(n-1)
+    return n * Factorial(n-1)
+
 
 def SqrtBisect(x, tolerance):
     return SqrtBisectRecurse(x, 0, x + 1, tolerance * tolerance)
+
 
 def SqrtBisectRecurse(x, a, b, tolerance):
     guess = (b+a)/2
@@ -36,12 +42,16 @@ def SqrtBisectRecurse(x, a, b, tolerance):
 
 
 print('Pascal Triangle:')
-for n in range(0,10):
-    line=''
-    for j in range(0,n+1):
-        line += str(Binomial(n,j))+' '
+for n in range(0, 40):
+    line = ''
+    for j in range(0, n + 1):
+        num = Binomial(n,j)
+        line += ('U') if num % 2 != 0 else ' '
     print(line.center(80))
 
 print(Factorial(5))
-print([Fibonacci(n) for n in range(0,10)])
+
+print(Binomial(5, 4))
+
+print([Fibonacci(n) for n in range(0, 10)])
 print(SqrtBisect(0.5, 0.001))
